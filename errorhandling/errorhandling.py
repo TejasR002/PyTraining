8e85b945db70d3d05d0d47924ede1d014f278cf5
Necromencer <tejas.raval@simformsolutions.com>
2025-01-20 10:45:52 +0530

    mergedchanges

--- errorhandling/errorhandling.py
+++ errorhandling/errorhandling.py
@@ -1,5 +1,4 @@
 try:
-<<<<<<< HEAD
   print(x)
 except NameError:
   print("Variable x is not defined")
@@ -14,11 +13,3 @@ except:
   print("Something went wrong")
 finally:
   print("The 'try except' is finished")
-=======
-    a = 10
-    b = 0
-    print(a / b)
-except:
-    print("You can not divide by zero")
-    
->>>>>>> 84a14338bbada5230530ce8cdbd84c90aaf96aff
